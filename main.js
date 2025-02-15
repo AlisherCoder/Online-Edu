e8127d08db475d04c6e618d2091ca523841e2fa7
unknown <ibrahimovkamronbek7@.com>
2025-02-15 11:06:59 +0500

    Multer was added by kamron

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import mainRoute from "./routes/index.js";
 import sequelize from "./config/db.js";
+import upload from "./config/multer.js";
 config();
 
 const PORT = process.env.PORT;
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", mainRoute);
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send(`Fayl yuklandi: ${req.file.filename}`);
+});
 
 async function connection() {
   try {
